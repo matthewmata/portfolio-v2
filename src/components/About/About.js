8cac170b4ccc8a16b5a5d89ee223ebcab0177c39
Matthew Mata <matthewmata1030@gmail.com>
2023-05-03 18:10:44 -0700

    fix: dynamic resizing issue for about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,19 +5,15 @@ const About = () => {
         <div className="row">
           <h4>
             Eager to work with others to find creative solutions to challenging
-            problems. Communicating complex technical concepts in a way that is
-            easy for others to understand. Always willing to put in the extra
-            effort to ensure success.
+            problems. Always willing to put in the extra effort to ensure
+            success.
           </h4>
           <div className="text-wrap">
             <p>
               <span>
-                My passionate about engineering, and the thrill of
-                designing, building, and problem-solving is what drives {" "}
-              </span>
-              <span>
-                me to pursue this field with dedication and enthusiasm.
-              </span>
+                My passionate about engineering, and the thrill of designing,
+                building, and problem-solving is what drives me to pursue {""}
+              this field with dedication and enthusiasm.</span>
             </p>
             <div className="about-me">About Me</div>
           </div>
